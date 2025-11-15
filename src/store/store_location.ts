6a548bec6754fbbcc3bef9bc9efa112796fbda2d
Maxim Butenko <maxim.butenko@proton.me>
2025-11-15 12:41:43 +0300

    rm debug

--- src/store/store_location.ts
+++ src/store/store_location.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { inject, ref, watch } from "vue";
+import { inject, ref } from "vue";
 import { useRequest } from "vue-hooks-plus";
 import type { Location, LocationClient } from "../api/api_location";
 import { locationClientKey } from "../injections";
@@ -21,10 +21,6 @@ export const useLocationStore = defineStore("location", () => {
     if (query.trim().length < 3) return;
     suggestedLocations.value = await runAsync(query);
   }
-
-  watch(currentLocation, (l) => {
-    console.log(l);
-  });
 
   return {
     loading,
